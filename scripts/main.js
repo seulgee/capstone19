6483a7d3e7ffa16afded68cf4ea0d25512a5edd8
Seulgee Lee <seulgee.lee97@gmail.com>
2019-03-19 23:57:32 -0500

    slider works perfectly

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
 
-    var about = $(".nav-about")
-    var aboutLink = $(".nav-about__link")
-    var why = $(".nav-why")
-    var whyLink = $(".nav-why__link")
+    var about = $(".nav-about__link")
+    var aboutLink1 = $(".about-letter1")
+    var aboutLink2 = $(".about-letter2")
+    var why = $(".nav-why__link")
+    var whyLink1 = $(".why-letter1")
+    var whyLink2 = $(".why-letter2")
+    var whyLink3 = $(".why-letter3")
     var phrase = $('#main-phrase')
     var translation = $('#translation')
     var slider = $('#slidercontainer, .slider-text')
 
+    //page load transitions
+
     $('.vl').animate({
         height: "100px"
-    },350)
+    },350);
 
-    $('#icon-audio').animate({opacity:1},300).delay(1400);
-    $('#icon-refresh').animate({opacity:1},300).delay(1600);
-    $('#icon-image').animate({opacity:1},300).delay(1800);
+    setTimeout(function(){
+        $('#icon-audio').animate({opacity:1},300)
+        setTimeout(function(){
+            $('#icon-refresh').animate({opacity:1},300)
+            setTimeout(function(){
+                $('#icon-image').animate({opacity:1},300)
+            },150)
+        },150)
+    },600)
 
-    translation.animate({
+    translation.delay(15).animate({
         opacity: '1'
     },500);
 
@@ -29,32 +40,83 @@ $(document).ready(function(){
         top: '0px'
     },400)
 
-    about.mouseover(function(){
-        TweenLite.to(aboutLink, 2, {text:"소개", ease:Power2.easeIn});
-    });
+
+    //nav items animation
+
+    about.mouseenter(function(){
+        aboutLink1.animate({
+            right: "15px"
+        }, 200)
+        console.log('anim works')
+        aboutLink2.animate({
+            opacity: 1 
+        }, 200)
+    })
 
     about.mouseleave(function(){
-        TweenLite.to(aboutLink, 2, {text:"소", ease:Power2.easeIn});
-    });
+        aboutLink1.animate({
+            right: "0px"
+        }, 200)
+        console.log('anim works')
+        aboutLink2.animate({
+            opacity: 0 
+        }, 200)
+    })
 
-    why.mouseover(function(){
-        TweenLite.to(whyLink, 2, {text:"중요성", ease:Power2.easeIn});
-    });
+    why.mouseenter(function(){
+        whyLink1.animate({
+            right: "30px"
+        }, 200)
+        console.log('anim works')
+        whyLink2.animate({
+            right: "15px",
+            opacity: 1 
+        }, 200)
+        whyLink3.animate({
+            opacity: 1 
+        }, 200)
+    })
 
     why.mouseleave(function(){
-        TweenLite.to(whyLink, 2, {text:"중", ease:Power2.easeIn});
-    });
+        whyLink1.animate({
+            right: "0px"
+        }, 200)
+        console.log('anim works')
+        whyLink2.animate({
+            right: "0px",
+            opacity: 0
+        }, 200)
+        whyLink3.animate({
+            opacity: 0
+        }, 200)
+    })
 
-    var fit = fitty('h1');
+    // about.mouseleave(function(){
+    //     aboutLink1.animate({
+    //         right: "0px"
+    //     }, 200)
+    //     console.log('anim works')
+    //     aboutLink2.animate({
+    //         opacity: 0 
+    //     }, 200)
+    // })
+
+
+    //image icon toggle function
+
+    var fitties = fitty('h1');
+    function delayFunction(){
+        fitty('h1');
+    };
     var phraseActual = phrase.find('h1')
     var imageIcon = $('#icon-image');
     var translationActual = translation.find('p');
     var sliderRow = $('#sliderrow');
+    var toggle = true;
 
-    var  toggle = true;
     imageIcon.on("click",function(){
         if (toggle) {
-            fit[0].unsubscribe();
+            fitties[0].unsubscribe();
             phraseActual.animate({
                 fontSize: "40px",
                 marginTop: "+190px"
@@ -85,9 +147,7 @@ $(document).ready(function(){
             $('.image-1').animate({
                 left: "-100%"
             },700);
-            $(function(){
-                fitty('h1');
-            },500).delay(200);
+            setTimeout(delayFunction, 550);
             console.log("works");
             toggle=!toggle;
         }
