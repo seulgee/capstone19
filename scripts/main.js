1dd0f76f33930424c537c8717b5e6c3b54b07785
Seulgee Lee <seulgee.lee97@gmail.com>
2019-03-31 22:52:27 -0500

    smooth fontsize animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-
-    var about = $(".nav-about__link")
-    var aboutLink1 = $(".about-letter1")
-    var aboutLink2 = $(".about-letter2")
-    var why = $(".nav-why__link")
-    var whyLink1 = $(".why-letter1")
-    var whyLink2 = $(".why-letter2")
-    var whyLink3 = $(".why-letter3")
+    getFirstFontSize();
     var phrase = $('#main-phrase')
     var translation = $('#translation')
     var slider = $('#slidercontainer, .slider-text')
@@ -25,7 +18,7 @@ $(document).ready(function(){
                 $('#icon-image').animate({opacity:1},300)
             },150)
         },150)
-    },600)
+    },400)
 
     translation.delay(15).animate({
         opacity: '1'
@@ -42,6 +35,9 @@ $(document).ready(function(){
 
 
     //nav items animation
+    var about = $(".nav-about__link")
+    var aboutLink1 = $(".about-letter1")
+    var aboutLink2 = $(".about-letter2")
 
     about.mouseenter(function(){
         aboutLink1.animate({
@@ -62,6 +58,11 @@ $(document).ready(function(){
             opacity: 0 
         }, 200)
     })
+
+    var why = $(".nav-why__link")
+    var whyLink1 = $(".why-letter1")
+    var whyLink2 = $(".why-letter2")
+    var whyLink3 = $(".why-letter3")
 
     why.mouseenter(function(){
         whyLink1.animate({
@@ -91,56 +92,130 @@ $(document).ready(function(){
         }, 200)
     })
 
-    //image icon toggle function
+    var index = $(".nav-index__link")
+    var indexLink1 = $(".index-letter1")
+    var indexLink2 = $(".index-letter2")
+    var indexLink3 = $(".index-letter3")
+    var indexLink4 = $(".index-letter4")
+    var indexLink5 = $(".index-letter5")
 
-    var fitties = fitty('h1');
-    function delayFunction(){
-        fitty('h1');
-    };
-    var phraseActual = phrase.find('h1')
-    var imageIcon = $('#icon-image');
-    var translationActual = translation.find('p');
-    var sliderRow = $('#sliderrow');
-    var toggle = true;
+    index.mouseenter(function(){
+        indexLink1.animate({
+            right: "60px"
+        }, 230)
+        console.log('anim works')
+        indexLink2.animate({
+            right: "45px",
+            opacity: 1 
+        }, 230)
+        indexLink3.animate({
+            right: "30px",
+            opacity: 1 
+        }, 230)
+        indexLink4.animate({
+            right: "15px",
+            opacity: 1 
+        }, 230)
+        indexLink5.animate({
+            opacity: 1 
+        }, 230)
+    })
 
-    imageIcon.on("click",function(){
-        if (toggle) {
-            fitties[0].unsubscribe();
-            phraseActual.animate({
-                fontSize: "40px",
-                marginTop: "+190px"
-            },400);
-            translationActual.animate({
-                fontSize: ".8em",
-                marginTop: "20px"
-            },400);
-            sliderRow.animate({
-                marginTop: "50px"
-            });
-            $('.image-1').delay(200).animate({
-                left: "0"
-            },700);
-            console.log("toggle");
-            toggle=!toggle;
-        } else {
-            phraseActual.delay(200).animate({
-                marginTop: "0px"
-            },400);
-            translationActual.animate({
-                fontSize: "1em",
-                marginTop: "0px"
-            },400);
-            sliderRow.animate({
-                marginTop: "25px"
-            });
-            $('.image-1').animate({
-                left: "-100%"
-            },700);
-            setTimeout(delayFunction, 550);
-            console.log("works");
-            toggle=!toggle;
-        }
-    });
+    index.mouseleave(function(){
+        indexLink1.animate({
+            right: "0px"
+        }, 230)
+        console.log('anim works')
+        indexLink2.animate({
+            right: "0px",
+            opacity: 0 
+        }, 230)
+        indexLink3.animate({
+            right: "0px",
+            opacity: 0
+        }, 230)
+        indexLink4.animate({
+            right: "0px",
+            opacity: 0
+        }, 230)
+        indexLink5.animate({
+            opacity: 0
+        }, 230)
+    })
+
+    // //image icon toggle function
+
+    // var fitties = fitty('h1');
+    // function delayFunction(){
+    //     fitty('h1');
+    //     setTimeout(getFontSize, 50);
+    // };
+    // var phraseActual = phrase.find('h1')
+    // var imageIcon = $('#icon-image');
+    // var translationActual = translation.find('p');
+    // var sliderRow = $('#sliderrow');
+    // var futureTarget = $('.future-change-en');
+    // var toggle = true;
+
+    // imageIcon.on("click",function(){
+
+    //     if (toggle) {
+    //         fitties[0].unsubscribe();
+    //         futureTarget.removeClass('trans');
+    //         phraseActual.animate({
+    //             fontSize: "40px",
+    //             marginTop: "+190px"
+    //         },400);
+    //         setTimeout(getFontSize, 400);
+    //         translationActual.animate({
+    //             fontSize: "19px",
+    //             marginTop: "20px"
+    //         },400);
+    //         sliderRow.animate({
+    //             marginTop: "50px"
+    //         });
+    //         $('.image-1').delay(200).animate({
+    //             left: "0"
+    //         },700);
+    //         console.log("toggle");
+    //         toggle=!toggle;
+    //     } else {
+    //         futureTarget.removeClass('trans');
+    //         phraseActual.delay(200).animate({
+    //             marginTop: "0px"
+    //         },400);
+    //         translationActual.animate({
+    //             fontSize: "25px",
+    //             marginTop: "0px"
+    //         },400);
+    //         sliderRow.animate({
+    //             marginTop: "25px"
+    //         });
+    //         $('.image-1').animate({
+    //             left: "-100%"
+    //         },700);
+    //         setTimeout(delayFunction, 550);
+            
+    //         //getFontSize();
+    //         console.log("works");
+    //         toggle=!toggle;
+    //     }
+        
+    // });
+
+    // function getFontSize (){
+    //     // const element = document.querySelector('#translationRow')
+    //     // const style = getComputedStyle(element)
+    //     // const fontSize = style.fontSize
+    //     // console.log(fontSize)
+    //     var translationActual = $('#translationRow');
+    //     var fontSize = translationActual.css("fontSize");
+    //     console.log(fontSize  = parseInt(fontSize) + 1 + "px")
+    //     translationActual.css("fontSize", fontSize );
+    // }
+
+
+    
 
 
 
